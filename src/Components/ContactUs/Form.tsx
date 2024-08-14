a1bdb00d7cad5199a955b6b0c6abf047e93987db
Marwan-Zakia <marwanamir.ma@gmail.com>
2024-08-15 01:19:35 +0300

    Update styling and layout for AboutUsCard, globals.css, ContactUs, and Heading components

--- src/Components/ContactUs/Form.tsx
+++ src/Components/ContactUs/Form.tsx
@@ -1,84 +1,127 @@
 "use client"
 
-import { Button, FormControl, InputLabel, NativeSelect, TextField } from '@mui/material'
+import { Box, Button, Fab, FormControl, InputLabel, NativeSelect, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Form() {
     return (
-        <div>
-            <FormControl fullWidth>
-                <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                    Age
-                </InputLabel>
-                <NativeSelect
-                    defaultValue={30}
-                    inputProps={{
-                        name: 'age',
-                        id: 'uncontrolled-native',
-                    }}
-                >
-                    <option value={10}>Ten</option>
-                    <option value={20}>Twenty</option>
-                    <option value={30}>Thirty</option>
-                </NativeSelect>
-            </FormControl>
-            <FormControl fullWidth>
-                <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                    Age
-                </InputLabel>
-                <TextField
-                    id="standard-textarea"
-                    label="Multiline Placeholder"
-                    placeholder="Placeholder"
-                    multiline
-                    variant="standard"
-                />
-            </FormControl>
-            <FormControl fullWidth>
-                <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                    Age
-                </InputLabel>
-                <TextField
-                    id="standard-textarea"
-                    label="Multiline Placeholder"
-                    placeholder="Placeholder"
-                    multiline
-                    variant="standard"
-                />
-            </FormControl>
-            <FormControl fullWidth>
-                <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                    Age
-                </InputLabel>
-                <TextField
-                    id="standard-textarea"
-                    label="Multiline Placeholder"
-                    placeholder="Placeholder"
-                    multiline
-                    variant="standard"
-                />
-            </FormControl>
-            <FormControl fullWidth>
-                <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                    Age
-                </InputLabel>
-                <TextField
-                    id="standard-textarea"
-                    label="Multiline Placeholder"
-                    placeholder="Placeholder"
-                    multiline
-                    variant="standard"
-                />
-            </FormControl>
-            <FormControl fullWidth>
-                <Button
-                    onClick={() => {
 
+        <Box>
+            <Typography sx={{ marginTop: "2rem", fontFamily: "Nobile", fontSize: { lg: "22px", md: "20px", xs: "15px" }, lineHeight: { lg: "40px", xs: "25px" }, fontWeight: 700, color: "#3F485E", maxWidth: "34rem", paddingBottom: "2rem" }}>Have a question?</Typography>
+            <Box sx={{
+                display: "grid",
+
+                gap: "2rem",
+                paddingBottom: "2rem"
+
+
+            }} >
+                <FormControl fullWidth sx={{ position: "relative" }}>
+                    <Typography sx={{ position: "absolute", top: "0px", left: "0", fontFamily: "Nobile", fontSize: "15px", color: "#3F485E", maxWidth: "34rem", paddingBottom: "2rem" }}>Types</Typography>
+                    <NativeSelect
+                        id='typesSelect'
+                        sx={{
+                            borderRadius: "25px",
+                            border: "1px solid #eee",
+                            padding: "0.75rem",
+                            boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                            marginTop: "1.8rem",
+                        }}
+                        defaultValue={"Careers"}
+                        inputProps={{
+                            name: 'type',
+                            id: 'uncontrolled-native',
+                        }}
+                    >
+                        <option value={"Careers"}>Career </option>
+                        <option value={"Complain"}>Complain</option>
+
+                    </NativeSelect>
+                </FormControl>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexBasis: "50",
+                        gap: "1rem"
                     }}
                 >
-                    Click me
-                </Button>
-            </FormControl>
-        </div>
+
+
+                    <FormControl fullWidth sx={{ position: "relative" }}>
+                        <Typography sx={{ position: "absolute", top: "0px", left: "0", fontFamily: "Nobile", fontSize: "15px", color: "#3F485E", maxWidth: "34rem", paddingBottom: "2rem" }}>Name</Typography>
+
+                        <TextField
+                            sx={{
+                                borderRadius: "25px",
+                                border: "1px solid #eee",
+                                padding: "0.5rem",
+                                boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                                marginTop: "1.8rem",
+                            }}
+
+
+
+                            variant="standard"
+                            defaultValue={"ali"}
+                        />
+                        <Typography sx={{ position: "absolute", bottom: "-20px", right: "2px", fontFamily: "Nobile", fontSize: "10px", color: "#3F485E", maxWidth: "34rem", }}>Optinal</Typography>
+                    </FormControl>
+                    <FormControl fullWidth sx={{ position: "relative" }}>
+                        <Typography sx={{ position: "absolute", top: "0px", left: "0", fontFamily: "Nobile", fontSize: "15px", color: "#3F485E", maxWidth: "34rem", paddingBottom: "2rem" }}>Email</Typography>
+
+                        <TextField
+                            sx={{
+                                borderRadius: "25px",
+                                border: "1px solid #eee",
+                                padding: "0.5rem",
+                                boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                                marginTop: "1.8rem",
+                            }}
+
+
+
+                            variant="standard"
+                        />
+                    </FormControl>
+                </Box>
+                <FormControl fullWidth sx={{ position: "relative" }}>
+                    <Typography sx={{ position: "absolute", top: "0px", left: "0", fontFamily: "Nobile", fontSize: "15px", color: "#3F485E", maxWidth: "34rem", paddingBottom: "2rem" }}>Message</Typography>
+                    <TextField
+                        sx={{
+                            borderRadius: "25px",
+                            border: "1px solid #eee",
+                            padding: "0.75rem",
+                            boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                            marginTop: "1.8rem",
+                        }}
+
+
+                        placeholder=""
+                        multiline
+                        variant="standard"
+                        defaultValue={"Content ?? "}
+                        rows={5}
+                    />
+                </FormControl>
+
+                <FormControl sx={{ width: "50%" }}>
+                    <Fab variant="extended" size='large' sx={{
+                        backgroundColor: "#3F485E", color: "#ffffff", fontSize: "12px", ":hover": {
+                            borderTop: "2px solid #3F485E",
+                            color: "#3F485E",
+                        },
+                        padding: "1.5rem",
+                        fontFamily: "Nobile"
+                    }}>
+
+                        Send
+                    </Fab>
+                </FormControl>
+
+            </Box>
+        </Box>
+
+
+
     )
 }
--- src/Components/ContactUs/Heading.tsx
+++ src/Components/ContactUs/Heading.tsx
@@ -1,13 +1,26 @@
+import { Box, Container, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Heading() {
     return (
-        <div>
-            <div className="">Contact Us</div>
-            <div className="">With lots of unique blocks, you can easily build a page easily without any coding. </div>
-            <div className="">Get in touch today!</div>
 
-        </div>
+        <Container maxWidth='lg' style={{ display: "grid" }}>
+            <Box sx={{
+
+                maxWidth: "30rem",
+                display: "flex",
+                justifyContent: "center",
+                alignContent: "center",
+                flexDirection: "column",
+                margin: { lg: "4rem auto", xs: "2rem auto" },
+                textTransform: "capitalize"
+            }}>
+
+                <Typography sx={{ fontFamily: "Nobile", fontSize: { lg: "30px", md: "20px", xs: "15px" }, lineHeight: { lg: "40px", xs: "25px" }, fontWeight: 700, color: "#3F485E", maxWidth: "34rem", textAlign: "center" }}>Contact Us </Typography>
+                <Typography sx={{ fontFamily: "Nobile", fontSize: { lg: "16px", md: "12px", xs: "12px" }, lineHeight: { lg: "30px", xs: "25px" }, fontWeight: 400, textAlign: "center" }}>With lots of unique blocks, you can easily build a page easily without any coding.</Typography>
+                <Typography sx={{ marginTop: "2rem", fontFamily: "Nobile", fontSize: { lg: "24px", md: "20px", xs: "15px" }, lineHeight: { lg: "40px", xs: "25px" }, fontWeight: 700, color: "#3F485E", maxWidth: "34rem", textAlign: "center" }}>Get in touch today!</Typography>
+            </Box>
+        </Container>
     )
 }
 
