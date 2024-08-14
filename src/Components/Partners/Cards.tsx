46b912588c35652fc389de2ac4791e2a2a9e6678
Marwan-Zakia <marwanamir.ma@gmail.com>
2024-08-15 00:18:43 +0300

    chore: Update Partners component styling and layout

--- src/Components/Partners/Cards.tsx
+++ src/Components/Partners/Cards.tsx
@@ -1,29 +1,103 @@
+import { Box, Container, Typography } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 
 export default function Cards() {
     return (
-        <div>
-            <div className="">
-                <Image alt='partner' src={"/partner.png"} width={237} height={212} />
-                <div className="">
-                    Agreement between xyz and ourwebsite
-                </div>
-            </div>
-            <div className="">
-                <Image alt='partner' src={"/partner.png"} width={237} height={212} />
-                <div className="">
-                    Agreement between xyz and ourwebsite
-                </div>
-            </div>
-            <div className="">
-                <Image alt='partner' src={"/partner.png"} width={237} height={212} />
-                <div className="">
-                    Agreement between xyz and ourwebsite
-                </div>
+        <div className="" >
+            <Container maxWidth="lg" sx={{
 
-            </div>
+            }} >
+                <Box sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "1rem",
+                    flexWrap: "wrap",
+                    flexBasis: "33.3"
 
+                }}>
+                    <Box sx={{
+                        backgroundColor: "#fff",
+                        padding: "2rem 1rem 5rem 1rem",
+                        display: "grid",
+                        placeContent: "center",
+                        placeItems: "center",
+                        rowGap: "1rem",
+                        borderRadius: "25px",
+                        position: "relative",
+                        height: "260px",
+                        ":after": {
+                            position: "absolute",
+                            content: "''",
+                            borderBottom: "7px solid  #EB6B2A90",
+                            width: "70%",
+                            transform: "translateX(-50%)",
+                            bottom: "0px",
+                            left: "50%",
+                            borderRadius: "25px 25px 0  0"
+                        }
+                    }} className="">
+                        <Image alt='partner' src={"/partner.png"} width={100} height={75} style={{ boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px" }} />
+                        <Typography sx={{ fontFamily: "Nobile", fontSize: { lg: "16px", md: "12px", xs: "12px" }, lineHeight: { lg: "30px", xs: "25px" }, fontWeight: 400, textAlign: "center", color: "#000" }}>
+                            Agreement between xyz and ourwebsite
+                        </Typography>
+                    </Box>
+                    <Box sx={{
+                        backgroundColor: "#fff",
+                        padding: "2rem 1rem 5rem 1rem",
+                        display: "grid",
+                        placeContent: "center",
+                        placeItems: "center",
+                        rowGap: "1rem",
+                        borderRadius: "25px",
+                        position: "relative",
+                        height: "260px",
+                        ":after": {
+                            position: "absolute",
+                            content: "''",
+                            borderBottom: "7px solid  #EB6B2A90",
+                            width: "70%",
+                            transform: "translateX(-50%)",
+                            bottom: "0px",
+                            left: "50%",
+                            borderRadius: "25px 25px 0  0"
+                        }
+                    }} className="">
+                        <Image alt='partner' src={"/partner.png"} width={100} height={75} style={{ boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px" }} />
+                        <Typography sx={{ fontFamily: "Nobile", fontSize: { lg: "16px", md: "12px", xs: "12px" }, lineHeight: { lg: "30px", xs: "25px" }, fontWeight: 400, textAlign: "center", color: "#000" }}>
+                            Agreement between xyz and ourwebsite
+                        </Typography>
+                    </Box>
+                    <Box sx={{
+                        backgroundColor: "#fff",
+                        padding: "2rem 1rem 5rem 1rem",
+                        display: "grid",
+                        placeContent: "center",
+                        placeItems: "center",
+                        rowGap: "1rem",
+                        borderRadius: "25px",
+                        position: "relative",
+                        height: "260px",
+                        ":after": {
+                            position: "absolute",
+                            content: "''",
+                            borderBottom: "7px solid  #EB6B2A90",
+                            width: "70%",
+                            transform: "translateX(-50%)",
+                            bottom: "0px",
+                            left: "50%",
+                            borderRadius: "25px 25px 0  0"
+                        }
+                    }} className="">
+                        <Image alt='partner' src={"/partner.png"} width={100} height={75} style={{ boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px" }} />
+                        <Typography sx={{ fontFamily: "Nobile", fontSize: { lg: "16px", md: "12px", xs: "12px" }, lineHeight: { lg: "30px", xs: "25px" }, fontWeight: 400, textAlign: "center", color: "#000" }}>
+                            Agreement between xyz and ourwebsite
+                        </Typography>
+                    </Box>
+                </Box>
+            </Container>
         </div>
     )
 }
+
--- src/Components/Partners/Index.tsx
+++ src/Components/Partners/Index.tsx
@@ -4,7 +4,7 @@ import Cards from './Cards'
 
 export default function Partners() {
     return (
-        <div>
+        <div style={{ background: "linear-gradient(151.81deg, #3F485E 24.5%, #49536B 53.53%, #535E78 82.55%)", paddingBottom: "6rem " }}>
             <Heading />
             <Cards />
         </div>
